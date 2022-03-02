1cdedb04079cf8ed18e30050a0dda20386d19fc1
iarmourgarb <74074190+iarmourgarb@users.noreply.github.com>
2022-03-01 19:27:14 -0500

    started making tables

--- django_app/tables_app/admin.py
+++ django_app/tables_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Artist, Rating
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    pass
