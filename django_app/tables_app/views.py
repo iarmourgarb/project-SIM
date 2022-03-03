b0f36d487b293da0f21daacfd082c38ce01dded4
Magda Kisielinska <mkisielinska@Magda-Kisielinska.local>
2022-03-03 10:44:14 -0500

    views

--- django_app/tables_app/views.py
+++ django_app/tables_app/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from .models import User, Rating, Artist
+from django.http import Http404
+
 
 # Create your views here.
+
+#index page
+def index(request):
+    context={}
+    if request.method == "POST":
+        #registration form
+        if request['registration']:
+            username = request['username']
+            password = request['password']
+            if username == "" and password == "":
+                context["error"] = "Empty username and password"
+            elif username == "":
+                context["error"] = "Empty username"
+            elif password == "":
+                context["error"] = "No password given"
+            else:
+                #if we have the text in there
+                try:
+                    user_check = User.objects.get(pk=username)
+                    if user_check:
+                        context["error"] = "Username already taken"
+                except:
+                    User.(username=username,password=password).save()
+                    context["success"] = "User " + username + " registered successfully"
+        #other form
+        elif request["song_retrival"]:
+            user = request['user']
+            if user == "":
+                context={"error:" : "No user given"}
+
+    #check if username is already there
+    try:
+        user_check = User.objects.get(pk=user)
+    except User.DoesNotExist:
+        context
+    try:
+        ratings= Rating.objects.filter(username=user)
+    return render(request, "tables-app/index.html",context)
+
+
+def index(request):
+    if request.method == 'POST':
+        username = request.get('username')
+        password = request.get('password')
+    else:
+    return render(request, 'index.html', {'request': request})
