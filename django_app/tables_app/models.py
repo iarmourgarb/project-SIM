4ca9fa9690016732eaf782e2155e35dc2da032ab
Sam Ephron <sephron@wesleyan.edu>
2022-03-08 16:16:16 -0500

    project done except for readme and cleaning up

--- django_app/tables_app/models.py
+++ django_app/tables_app/models.py
@@ -6,6 +6,9 @@ class User(models.Model):
     username = models.CharField(max_length = 255, primary_key = True)
     password = models.CharField(max_length = 255)
 
+    def str_user(self):
+        return self.username
+
 class Artist(models.Model):
     song = models.CharField(max_length = 255, primary_key = True)
     artist = models.CharField(max_length = 255)
@@ -20,7 +23,7 @@ class Rating(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     song = models.ForeignKey(Artist, on_delete = models.CASCADE)
     rating = models.IntegerField(default= 1)
-    rate_date = models.DateTimeField(default=datetime.now)
+    rate_date = models.DateField(default=datetime.now)
 
     def str_song(self):
         return self.song.str_song()
@@ -33,15 +36,18 @@ class Rating(models.Model):
 
 class Review(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
-    song = song = models.ForeignKey(Artist, on_delete = models.CASCADE)
+    song = models.ForeignKey(Artist, on_delete = models.CASCADE)
     review = models.CharField(max_length=510)
-    review_date = models.DateTimeField(default=datetime.now)
+    review_date = models.DateField(default=datetime.now)
+
+    def str_user(self):
+        return self.username.str_user()
 
     def str_song(self):
         return self.song.str_song()
 
-    def str_rating(self):
-        return self.rating.str_rating()
+    def str_review(self):
+        return self.review
 
-    def str_rate_date(self):
-        return str(self.rate_date)
+    def str_review_date(self):
+        return str(self.review_date)
--- django_app/tables_app/views.py
+++ django_app/tables_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import User, Rating, Artist
+from .models import User, Rating, Artist, Review
 from django.http import Http404
 
 
@@ -34,18 +34,19 @@ def index(request):
             context["type"] = "song_retrieval"
             user = request.POST['user']
             if user == "":
-                context={"error:" : "No user given"}
-            try:
-                user_check = User.objects.get(pk=user)
-                ratings = Rating.objects.filter(username=user)
-                if len(ratings) == 0:
-                    context["error"] = "User has not rated any songs"
-                printout = []
-                for i in ratings:
-                    printout.append(i.str_song() + " --> " + i.str_rating())
-                context["ratings"] = printout
-            except:
-                context["error"] = "Unknown User "
+                context["error"] = "No user given"
+            else:
+                try:
+                    user_check = User.objects.get(pk=user)
+                    ratings = Rating.objects.filter(username=user)
+                    if len(ratings) == 0:
+                        context["error"] = "User has not rated any songs"
+                    printout = []
+                    for i in ratings:
+                        printout.append(i.str_song() + " --> " + i.str_rating() + "; " + i.str_rate_date())
+                    context["ratings"] = printout
+                except:
+                    context["error"] = "Unknown User "
 
         elif "reviews" in request.POST:
             context["type"] = "review_retrieval"
@@ -53,7 +54,13 @@ def index(request):
             if date == "":
                 context['error'] = "No date provided"
             else:
-                
-
+                reviews = Review.objects.filter(review_date=date)
+                if len(reviews) == 0:
+                    context['error'] = "No reviews written on " + date
+                else:
+                    printout = []
+                    for i in reviews:
+                        printout.append(i.str_user() + ": " + i.str_song() + " --> " + i.str_review())
+                    context['reviews'] = printout
             
     return render(request, "tables_app/index.html",context)
