8f2fabb4a8c72a679e9a102add41b6fc919dd9a6
iarmourgarb <74074190+iarmourgarb@users.noreply.github.com>
2022-04-04 16:19:34 -0400

    frontend stuff

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,4 +1,8 @@
 // frontend/src/components/Modal.js
+// component for listing songs
+// renamed event to newSong
+// renamed activeItem to adding
+
 
 import React from "react";
 // We would like to use a modal (small window) to show details of a task.
@@ -17,7 +21,7 @@ import {
 // Let's create a component.
 // We are creating and exporting the CustomModal class. It is a class component.
 // It will be imported in our App.js.
-export default class CustomModal extends React.Component {
+export default class addSongs extends React.Component {
   // The constructor function will be called when the component gets initialized.
   // It is also the place where you initialize the component's properties,
   // aka props (that is, function arguments in React-speak).
@@ -33,7 +37,7 @@ export default class CustomModal extends React.Component {
     // Here we define one property called activeItem.
     // 'this' refers to the currently instantiated CustomModal.
     this.state = {
-      activeItem: this.props.activeItem,
+      adding: this.props.adding,
     };
   }
 
@@ -42,14 +46,20 @@ export default class CustomModal extends React.Component {
   // the handleChange function is called below with onChange={this.handleChange}.
   // handleChange takes an event argument, i.e., a change in title, descripton,
   // or checkbox.
-  handleChange = (event) => {
+  handleChange = (newSong) => {
     // An event has a target, and event.target gives us the event's DOM element, e.g., see further below
     // <input name="description" placeholder="Enter Todo description" type="text" class="form-control" value="My Task">.
     // To refer to the description we assign the variable name = "description" and value = "My Task".
-    let { name, value } = event.target;
-    if (event.target.type === "checkbox") {
-      value = event.target.checked;
+
+//name is song or artist, value is if we're adding the song or the artist
+    let { name, value } = newSong.target;
+
+    //something else has to change here
+    if (newSong.target.type === "checkbox") {
+       value = event.target.checked;
     }
+
+
     // Below we have the spread operator three dots ... . It works as follows:
     // var parts = ['two', 'three']; // [two", "three"]
     // var numbers = ['one', ...parts, 'four']; // ["one", "two", "three", "four"]
@@ -57,12 +67,12 @@ export default class CustomModal extends React.Component {
     // {"id": 3, "title": "My Task", "description": "Wash Dishes", "completed": true}.
     // [name]: value sets the name of the task to the new value the user entered
     // (e.g., title to "Another Task" or description to "Sweep floor").
-    const activeItem = { ...this.state.activeItem, [name]: value };
+    const newSong = { ...this.state.adding, [name]: value };
     // To change a value in the `state` object for rendering, use the `setState()`
     // method (on the current CustomModal instance referred to with `this`).
     // If you would not do so, the text shown to the user in the textbox, e.g.,
     // for the title of the new task would not change.
-    this.setState({ activeItem });
+    this.setState({ adding });
   };
   // The `render()` method is the only required method in a class component.
   // When called, it will render the page. You do not have to specifically
@@ -80,46 +90,51 @@ export default class CustomModal extends React.Component {
       // Open the modal on toggling/clicking. See the toggle function in App.js
       // below.
       <Modal isOpen={true} toggle={toggle}>
-        <ModalHeader toggle={toggle}> Todo Item </ModalHeader>
+        <ModalHeader toggle={toggle}> Adding Songs </ModalHeader>
         <ModalBody>
           <Form>
+
             <FormGroup>
-              <Label for="title">Title</Label>
+              <Label for="song">Song</Label>
               <Input
                 type="text"
-                name="title"
-                value={this.state.activeItem.title}
+                name="song"
+                value={this.state.adding.song}
                 // "this" refers to the current event. If there is a change,
                 // it will be passed to the handleChange function above.
                 onChange={this.handleChange}
-                placeholder="Enter Todo Title"
+                placeholder="Enter song name"
               />
             </FormGroup>
+
             <FormGroup>
-              <Label for="description">Description</Label>
+              <Label for="artist">Artist</Label>
               <Input
                 type="text"
-                name="description"
-                value={this.state.activeItem.description}
+                name="artist"
+                value={this.state.adding.artist}
                 onChange={this.handleChange}
-                placeholder="Enter Todo description"
+                placeholder="Enter artist name"
               />
             </FormGroup>
+
+// here we need a button to add to database??
             <FormGroup check>
               <Label for="completed">
                 <Input
                   type="checkbox"
                   name="completed"
-                  checked={this.state.activeItem.completed}
+                  checked={this.state.adding.completed}
                   onChange={this.handleChange}
                 />
                 Completed
               </Label>
             </FormGroup>
+
           </Form>
         </ModalBody>
         <ModalFooter>
-          <Button color="success" onClick={() => onSave(this.state.activeItem)}>
+          <Button color="success" onClick={() => onSave(this.state.adding)}>
             Save
           </Button>
         </ModalFooter>
