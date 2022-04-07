6e1230f7503821471bba4082385e09250105d0bd
Magda Kisielinska <mkisielinska@wesleyan.edu>
2022-04-07 14:43:41 -0400

    frontend?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React from "react";
+import Create from "./components/Create";
+import CreateSong from "./components/Create";
 
 function App() {
+
+  // handleSubmit = (formData) => {
+  //   // make axios request to mybackend.com/newSong, with context of username=songUsername
+  //   axios.post('localhostadsads/createSong', {
+  //   username: songUsername,
+  //   artist: songArtist
+  // })
+  // .then(function (response) {
+  //   console.log(response);
+  // })
+  // .catch(function (error) {
+  //   console.log(error);
+  // });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  //   <CreateSong handleSubmit={handleSubmit}/>
+      <CreateSong/>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ frontend/src/components/Create.js
@@ -0,0 +1,99 @@
+import React, {useState} from "react";
+import axios from "axios";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
+
+// This form will be for adding a song with a writing into the database
+
+function CreateSong(props) {
+  const [songUsername, setSongUsername] = useState("Mags")
+  const [songTitle, setSongTitle] = useState("Happy")
+  const [songArtist, setSongArtist] = useState("Pharrel")
+  const [songRating, setSongRating] = useState("5")
+
+  const handleSubmit = (formData) => {
+    // make axios request to mybackend.com/newSong, with context of username=songUsername
+    console.log("submitted")
+
+    axios.post('http://localhost:8000/api/artists', {
+    username: songUsername,
+    artist: songArtist,
+    title:songTitle,
+    rating:songRating
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  }
+  return (
+
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={songUsername} onChange={e => setSongUsername(e.target.value)}/>
+      </label>
+      <label>
+        Song Title:
+        <input type="text" value={songTitle} onChange={e => setSongTitle(e.target.value)} />
+      </label>
+      <label>
+        Artist:
+        <input type="text" value={songArtist} onChange={e => setSongArtist(e.target.value)} />
+      </label>
+      <label>
+        Rating:
+        <input type="text" value={songRating} onChange={e => setSongRating(e.target.value)} />
+      </label>
+      <input type="submit" value="Submit" onClick={() => handleSubmit("")}/>
+    </form>
+  );
+}
+
+export default CreateSong;
+
+//
+// // code from the Internet
+//
+// class NameForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {value: ''};
+//
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//
+//   handleChange(event) {
+//     this.setState({value: event.target.value});
+//   }
+//
+//   handleSubmit(event) {
+//     alert('A name was submitted: ' + this.state.value);
+//     event.preventDefault();
+//   }
+//
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Name:
+//           <input type="text" value={this.state.value} onChange={this.handleChange} />
+//         </label>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     );
+//   }
+// }
