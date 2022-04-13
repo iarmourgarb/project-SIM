d8321fab692db6c8f0065e0a5267c0cdd8968649
Magda Kisielinska <mkisielinska@wesleyan.edu>
2022-04-12 22:41:33 -0400

    delete button

---
+++ frontend/src/components/Delete.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+
+
+function DeleteButton(props) {
+  return (
+    // <div className="content">
+    <div>
+      <button
+        // className="button"
+        onClick={(e) => {
+          e.preventDefault();
+          axios.delete("http://localhost:8000/api/artists/", {props});
+        }}
+      >
+        Delete
+      </button>
+      {/* </a> */}
+    </div>
+  );
+}
+
+export default DeleteButton;
