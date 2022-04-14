366b2b33d78fffa93a94a319236ad357be5bedfb
Sam Ephron <sephron@wesleyan.edu>
2022-04-13 21:45:44 -0400

    find user

--- backend/tables/settings.py
+++ backend/tables/settings.py
@@ -134,7 +134,5 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ]
+        'rest_framework.authentication.TokenAuthentication',    ]
 }
